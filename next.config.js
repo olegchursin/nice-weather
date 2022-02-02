fd01de7b0047af1e0f9292b5dfd4cc45ea94fa21
Oleg Chursin <olegchursin@gmail.com>
2022-02-02 02:15:06 -0500

    fix: add base layout

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['cdn.weatherapi.com']
+  },
   env: {
     WEATHER_API: process.env.WEATHER_API,
     API_KEY: process.env.API_KEY
